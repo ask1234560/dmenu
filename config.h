06978fec34fba8012a6c1036c5d888abad8f4e36
ananthu <ask1234560@gmail.com>
2024-12-29 23:58:52 +0100

    updates

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "Fira Code:style=Medium:size=10"
+    "Fira Code:style=Medium:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 27;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 29;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
